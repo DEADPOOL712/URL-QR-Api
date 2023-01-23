1d2a545c5686fc2288fdc03d64f08f57008dbe49
DEADPOOL712 <98164944+DEADPOOL712@users.noreply.github.com>
2023-01-23 16:57:34 +0530

    ns

--- app.js
+++ app.js
@@ -12,7 +12,8 @@ app.get("/qrcode", (req, res) => {
     res.render("index", { qrcode: url });
   });
 });
+const port = process.env.PORT || 3000;
 
-app.listen(3000, () => {
+app.listen(port, () => {
   console.log("Server listening on port 3000");
 });
